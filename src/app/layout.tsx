989b6ec21b5d84053acc5d69741b3d3ff8c1c141
Erin Lee <skaurd12323@gmail.com>
2025-07-25 14:25:45 +0900

    favicon 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: '그린판다 파트너스 사전 등록',
   description: '회원권 거래의 새로운 기준, 그린판다 파트너스와 함께하세요',
+  icons: {
+    icon: '/favicon.svg'
+  }
 }
 
 export default function RootLayout({
@@ -20,6 +23,7 @@ export default function RootLayout({
           crossOrigin="" 
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css" 
         />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body style={{ fontFamily: '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif' }}>
         {children}
